1f19ee32f9f7b83fb46c4489ee934719e3ed3b3b
Deodat Kimuene <dkimuene1@gmail.com>
2022-08-14 15:49:25 -0500

    style the park result page

--- src/components/Park/index.js
+++ src/components/Park/index.js
@@ -1,13 +1,16 @@
-const Park = () => {
+import { useState } from "react";
+
+const Park = (fullName,images,description,entranceFee) => {
+    const [readMore, setReadMore] = useState(false);
     return ( 
-        <article className="single-park">
-            <img src={image} alt={`${name}`} />
+        <article key={park.id} className="single-park">
+            <img src={images[0].url} alt={`${fullName}`} />
             <footer>
                 <div className="tour-info">
-                    <h4>{name}</h4>
-                    <h4 className="tour-price">${price}</h4>
+                    <h4>{fullName}</h4>
+                    <h4 className="tour-price">${entranceFee[2].cost}</h4>
                 </div>
-                <p>{readMore? info:`${info.substring(0,200)}...`
+                <p>{readMore? description:`${description.substring(0,200)}...`
                     }
                     <button onClick={() => {setReadMore(!readMore)}}>{readMore?"show less":"read more"}</button>
                 </p>
