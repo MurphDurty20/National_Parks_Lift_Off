672e233b947d00bf8cf228d5c00cb0dfd755f317
Jon Murphy <Jonmurphy20@gmail.com>
2022-09-07 18:16:57 -0500

    Routing and cleanup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import './App.css';
-import SearchResults from './components/SearchResults';
-import RegistrationPage from './components/RegistrationPage';
-import Sidebar from './SideNav/Sidebar';
+//import Sidebar from './SideNav/Sidebar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { useEffect } from 'react';
 import Search from './search';
-import MapSection from './components/map/map.jsx'
-import Parks from './Parks.js';
-import Alerts from './Alerts.js';
+import Home from './components/Pages/Home/Home';
+import ResultTemplate from './components/Pages/Home/ResultTemplate/index';
 
-const location = {
-  address: 'Default location, change to search',
-  lat: 37.42216,
-  lng: -100,
-}
+
 
 const App = () => { 
   return (
     <div className="App">
-
-      <Sidebar />
+      <Router>
       <h1> National Parks </h1>
-      <MapSection location={location} zoomLevel={17} />
       <Search />
-      <Parks />
-      <Alerts />
-      {/* <Router>
-        <Sidebar />
         <Routes>
-          <Route path='/' />
-          <Route path="search" element={<SearchResults/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/ResultTemplate" element={<ResultTemplate />} />
         </Routes>
-      </Router> */}
+     </Router>
     </div>
   )
 }
